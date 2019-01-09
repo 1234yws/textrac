767fc85e9998eefcf867d4a03bb96e6bda5f84b4
袁伟森 <14385788@qq.com>
2019-01-09 17:43:00 +0800

    Initial Commit

---
+++ textrac/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  textrac
+//
+//  Created by 袁伟森 on 2019/1/9.
+//  Copyright © 2019 袁伟森. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ textrac/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  textrac
+//
+//  Created by 袁伟森 on 2019/1/9.
+//  Copyright © 2019 袁伟森. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
